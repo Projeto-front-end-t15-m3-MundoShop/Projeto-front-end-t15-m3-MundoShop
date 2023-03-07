a81e6e404f9a720f379d1412931e7f666c04c421
Miguel <miguelangeloliveira95@gmail.com>
2023-03-07 15:09:03 -0300

    corretion login/register

--- src/components/LoginForm/index.tsx
+++ src/components/LoginForm/index.tsx
@@ -2,6 +2,7 @@ import { useContext } from "react";
 import { SubmitHandler, useForm } from "react-hook-form";
 import { ILoginFormValues, UserContext } from "../../providers/UserContext";
 import Input from "../Input";
+import { ButtonLogin, FormLogin } from "./style";
 
 export const LoginForm = () => {
   const {
@@ -17,23 +18,27 @@ export const LoginForm = () => {
   };
 
   return (
-    <form onClick={handleSubmit(submit)}>
-      <h2>Bem vindo de volta!</h2>
-      <Input
-        label="Seu e-mail"
-        type="email"
-        register={register("email")}
-        errors={errors.email}
-      />
-      <Input
-        label="Sua senha"
-        type="password"
-        register={register("password")}
-        errors={errors.password}
-      />
-      <p>Não possui uma conta?</p>
-      <button>Entrar</button>
-      <button>Voltar para o início</button>
-    </form>
+    <>
+      <FormLogin onClick={handleSubmit(submit)}>
+        <h2>Bem vindo de volta!</h2>
+        <Input
+          label="Seu e-mail"
+          type="email"
+          register={register("email")}
+          errors={errors.email}
+        />
+        <Input
+          label="Sua senha"
+          type="password"
+          register={register("password")}
+          errors={errors.password}
+        />
+        <p>Não possui uma conta?</p>
+        <ButtonLogin>
+          <button>Entrar</button>
+          <button>Voltar para o início</button>
+        </ButtonLogin>
+      </FormLogin>
+    </>
   );
 };
---
+++ src/components/LoginForm/style.ts
@@ -0,0 +1,18 @@
+import styled from "styled-components";
+
+export const FormLogin = styled.form`
+  max-width: 502px;
+  width: 100%;
+  height: 585px;
+  background: #ffffff;
+  border-radius: 10px;
+
+  margin: 250px auto;
+`;
+
+export const ButtonLogin = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+export const ButtonEntrar = styled.button``;
