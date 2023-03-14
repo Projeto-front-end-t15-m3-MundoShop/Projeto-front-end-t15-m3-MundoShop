28e5b0ad5c578576a039cdcd6963dda76a9bfadb
Alyss0n-F <Alyss0n_F@hotmail.com>
2023-03-14 15:16:57 -0300

    fix: css dashboard

--- src/components/MySalesCard/index.tsx
+++ src/components/MySalesCard/index.tsx
@@ -1,28 +1,32 @@
 import { useContext } from "react"
 import { ProductsContext } from "../../providers/productsContext"
-import { StyledMyCardLi } from "./style"
+import { StyledMyCardLi, StyledMySalesCardContainerButton } from "./style"
 
 const MySalesCard = () => {
-    const {mySales, addImgToProduct} = useContext(ProductsContext)
- 
+  const { mySales, addImgToProduct } = useContext(ProductsContext)
+
   return (
     <>
-    {mySales?.map(item => (
+      {mySales?.map((item) => (
         <StyledMyCardLi key={item.id}>
-          <div>
-            {item.img ? <img src={item.img} alt={item.name}/> : null}
+          <div className="container__image">
+            <div>
+              {item.img ? <img src={item.img} alt={item.name} /> : null}
+            </div>
+            <div>
+              <h2>{item.name}</h2>
+              <p>{item.category}</p>
+              <p className="card__price">R${item.price}</p>
+            </div>
           </div>
-          <div>
-            <h2>{item.name}</h2>
-            <p>{item.category}</p>
-            <p>R${item.price}</p>
-          </div>
-          <div>
-            <button onClick={() => addImgToProduct(item.id)} >Adicionar fotos</button>
-            <button>Remover anúncio</button>
-          </div>
+          <StyledMySalesCardContainerButton>
+            <button onClick={() => addImgToProduct(item.id)}>
+              Adicionar fotos
+            </button>
+            <button className="remove__sale">Remover anúncio</button>
+          </StyledMySalesCardContainerButton>
         </StyledMyCardLi>
-    ))}
+      ))}
     </>
   )
 }
--- src/pages/MySales/index.tsx
+++ src/pages/MySales/index.tsx
@@ -7,57 +7,38 @@ import { ProductsContext } from "../../providers/productsContext"
 import { UserContext } from "../../providers/UserContext"
 import { api } from "../../Services/api"
 import { StyledHeaderDashboard } from "../Dashboard/style"
-import { StyledMySalesHeaderDiv, StyledMySalesUl } from "./style"
+import {
+  StyledMySalesHeaderDiv,
+  StyledMySalesMain,
+  StyledMySalesUl,
+} from "./style"
 
 export const MySalesPage = () => {
-  const {getUser} = useContext(UserContext)
+  const { getUser } = useContext(UserContext)
   const { setMySales, mySales } = useContext(ProductsContext)
   const navigate = useNavigate()
 
   useEffect(() => {
     const token = localStorage.getItem("@TOKEN")
-    const userId = localStorage.getItem('@USERID')
+    const userId = localStorage.getItem("@USERID")
     if (token) {
       getUser()
     } else {
-      navigate("/login");
+      navigate("/login")
     }
 
     const mySales = async () => {
-      try{
+      try {
         const response = await api.get(`/products?userId=${userId}`)
         setMySales(response.data)
-      }catch (error){
+      } catch (error) {
         console.log(error)
       }
     }
     mySales()
   }, [])
 
-  if(mySales.length === 0){
-    return (
-      <>
-      <StyledHeaderDashboard>
-        <LogoMundoShop />
-        <nav>
-          <a href="/">Início</a>
-          <p onClick={() => history.back()}>Voltar</p>
-        </nav>
-      </StyledHeaderDashboard>
-      <main>
-          <StyledMySalesHeaderDiv>
-            <h2>Meus anúncios</h2>
-          </StyledMySalesHeaderDiv>
-          <div>
-              <StyledMySalesUl>
-                <p>Sem anúncios cadastrados</p>
-              </StyledMySalesUl>
-          </div>
-          <MySalesImgModal/>
-      </main>
-    </>
-    )
-  }else{
+  if (mySales.length === 0) {
     return (
       <>
         <StyledHeaderDashboard>
@@ -67,17 +48,48 @@ export const MySalesPage = () => {
             <p onClick={() => history.back()}>Voltar</p>
           </nav>
         </StyledHeaderDashboard>
-        <main>
-            <StyledMySalesHeaderDiv>
-              <h2>Meus anúncios</h2>
-            </StyledMySalesHeaderDiv>
-            <div>
-                <StyledMySalesUl>
-                  <MySalesCard/>
-                </StyledMySalesUl>
-            </div>
-            <MySalesImgModal/>
-        </main>
+        <StyledMySalesMain>
+          <StyledMySalesHeaderDiv>
+            <img
+              src="./src/assets/botaoVoltar.png"
+              onClick={() => history.back()}
+            />
+            <h2>Minhas vendas</h2>
+          </StyledMySalesHeaderDiv>
+          <div>
+            <StyledMySalesUl>
+              <p>Sem anúncios cadastrados</p>
+            </StyledMySalesUl>
+          </div>
+          <MySalesImgModal />
+        </StyledMySalesMain>
+      </>
+    )
+  } else {
+    return (
+      <>
+        <StyledHeaderDashboard>
+          <LogoMundoShop />
+          <nav>
+            <a href="/">Início</a>
+            <p onClick={() => history.back()}>Voltar</p>
+          </nav>
+        </StyledHeaderDashboard>
+        <StyledMySalesMain>
+          <StyledMySalesHeaderDiv>
+            <img
+              src="./src/assets/botaoVoltar.png"
+              onClick={() => history.back()}
+            />
+            <h2>Minhas vendas</h2>
+          </StyledMySalesHeaderDiv>
+          <div>
+            <StyledMySalesUl>
+              <MySalesCard />
+            </StyledMySalesUl>
+          </div>
+          <MySalesImgModal />
+        </StyledMySalesMain>
       </>
     )
   }
