c3cb1993af60101b4a460c7adb0182afca6907d0
Jose Pedro Bernardes <josepedro.bernardes1@gmail.com>
2023-03-14 10:31:19 -0300

    feat: adicionando id e categoria na interface dos produtos.

--- src/providers/productsContext.tsx
+++ src/providers/productsContext.tsx
@@ -2,52 +2,62 @@ import { createContext, useEffect, useState } from "react";
 import { api } from "../Services/api";
 
 interface IDefaultProviderProps {
-    children: React.ReactNode;
+  children: React.ReactNode;
 }
 
 export interface IProducts {
-    description: string
-    img: string
-    name: string
-    price: number
-    userId: number
-    
+  description: string;
+  img: string;
+  name: string;
+  price: number;
+  userId: number;
+  category: string;
+  id: number;
 }
 
 interface IProductsContext {
-    list: IProducts[]
-    setList: React.Dispatch<React.SetStateAction<IProducts[]>>
-    filteredProducts: string
-    setFilteredProducts: React.Dispatch<React.SetStateAction<string>>
-    searchProducts: IProducts[]
+  list: IProducts[];
+  setList: React.Dispatch<React.SetStateAction<IProducts[]>>;
+  filteredProducts: string;
+  setFilteredProducts: React.Dispatch<React.SetStateAction<string>>;
+  searchProducts: IProducts[];
 }
 
 export const ProductsContext = createContext({} as IProductsContext);
 
 export const ProductsProvider = ({ children }: IDefaultProviderProps) => {
+  const [filteredProducts, setFilteredProducts] = useState("");
+  const [list, setList] = useState([] as IProducts[]);
 
-    const [filteredProducts, setFilteredProducts] = useState("")
-    const [list, setList] = useState([] as IProducts[])
+  useEffect(() => {
+    const ListProduct = async () => {
+      try {
+        const response = await api.get(`/products`);
+        setList(response.data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    ListProduct();
+  });
 
-    useEffect(() => {
-        const ListProduct = async () => {
-            try {
-                const response = await api.get(`/products`)
-                setList(response.data)
-            } catch (error) {
-                console.log(error)
-            }
-        }
-        ListProduct()
-    })
+  const searchProducts = list.filter((product) => {
+    return filteredProducts === ""
+      ? true
+      : product.name.toLowerCase().includes(filteredProducts.toLowerCase());
+  });
 
-    const searchProducts = list.filter((product) => {
-        return filteredProducts === "" ? true : (product.name.toLowerCase()).includes(filteredProducts.toLowerCase())
-    })
-
-    return (
-        <ProductsContext.Provider value={{ list, setList, filteredProducts, setFilteredProducts, searchProducts}}>
-            {children}
-        </ProductsContext.Provider>
-    );
+  return (
+    <ProductsContext.Provider
+      value={{
+        list,
+        setList,
+        filteredProducts,
+        setFilteredProducts,
+        searchProducts,
+      }}
+    >
+      {children}
+    </ProductsContext.Provider>
+  );
 };
