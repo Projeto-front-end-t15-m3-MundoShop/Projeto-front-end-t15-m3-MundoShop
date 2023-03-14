0d941cd269366802ee469979be7a86cc4b02e667
Jose Pedro Bernardes <josepedro.bernardes1@gmail.com>
2023-03-14 10:37:33 -0300

    Adicionando provider do CartContext na Pagina pricipal  e na pagina do carrinho.

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -4,12 +4,28 @@ import Dashboard from "./pages/Dashboard";
 import { LoginPage } from "./pages/LoginPage";
 import { MainPage } from "./pages/MainPage";
 import { RegisterPage } from "./pages/RegisterPage";
+import { CartProvider } from "./providers/CartContext";
 
 export const Router = () => {
   return (
     <Routes>
-      <Route path="/" element={<MainPage />} />
-      <Route path="/cart" element={<Cart />} />
+      <Route
+        path="/"
+        element={
+          <CartProvider>
+            <MainPage />
+          </CartProvider>
+        }
+      />
+      <Route
+        path="/cart"
+        element={
+          <CartProvider>
+            <Cart />
+          </CartProvider>
+        }
+      />
+
       <Route path="/register" element={<RegisterPage />} />
       <Route path="/dashboard" element={<Dashboard />} />
       <Route path="/login" element={<LoginPage />} />
