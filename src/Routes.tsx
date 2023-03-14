32cb8974d9cf4cb4cc1d2d2a3f7348963c9953de
Jose Pedro Bernardes <josepedro.bernardes1@gmail.com>
2023-03-14 08:49:13 -0300

    feat: Adicionando Rota do carrinho de compras

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -1,4 +1,5 @@
 import { Route, Routes } from "react-router-dom";
+import { Cart } from "./pages/Cart";
 import Dashboard from "./pages/Dashboard";
 import { LoginPage } from "./pages/LoginPage";
 import { MainPage } from "./pages/MainPage";
@@ -7,7 +8,8 @@ import { RegisterPage } from "./pages/RegisterPage";
 export const Router = () => {
   return (
     <Routes>
-      <Route path="/" element={<MainPage />}/>
+      <Route path="/" element={<MainPage />} />
+      <Route path="/cart" element={<Cart />} />
       <Route path="/register" element={<RegisterPage />} />
       <Route path="/dashboard" element={<Dashboard />} />
       <Route path="/login" element={<LoginPage />} />
